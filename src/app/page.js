9a2f7275223a6d675e468656438e2af013efb5bc
All Fine Maulinaro <allfinem10@gmail.com>
2025-01-01 17:58:21 +0700

    loading component

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import { tmbdConfig } from "./lib/config";
 const Home = async ({ searchParams }) => {
   const genre = searchParams?.genre || "fetchTrending";
   const res = await fetch(
-    `${tmbdConfig.endPointUrl}${genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"}?api_key=${tmbdConfig.apiKey}&language=en-US&page=1`
+    `${tmbdConfig.endPointUrl}${genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"}?api_key=${tmbdConfig.apiKey}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
 
   const data = await res.json();
